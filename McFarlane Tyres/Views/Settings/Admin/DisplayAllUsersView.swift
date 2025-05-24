0894a09c764cfc2aa05c7179689edf39e7f9ea04
Daniel <mcfarlanedaniel95@gmail.com>
2025-05-24 14:59:02 +0100

    fix: admin could delete all users in display all users view Changed it so the admin can only remove other admins and staff e.g. an employee leaves. Added a current user icon and made it so the current account can't be removed. Added a total users count.

--- McFarlane Tyres/Views/Settings/Admin/DisplayAllUsersView.swift
+++ McFarlane Tyres/Views/Settings/Admin/DisplayAllUsersView.swift
@@ -12,10 +12,12 @@ import SwiftUI
 
 struct DisplayAllUsersView: View {
     @Environment(\.modelContext) private var context
-    @State private var users: [User] = []
+    @Query private var users: [User]
     @State private var userToDelete: User?
     @State private var showDeleteConfirmation = false
-
+    
+    var loggedInUser: User? { users.first(where: { $0.loggedIn }) }
+    
     var body: some View {
         ZStack {
             Color(UIColor.systemGroupedBackground).ignoresSafeArea()
@@ -28,30 +30,55 @@ struct DisplayAllUsersView: View {
                         .padding()
                 } else {
                     List {
-                        // Display all users
-                        ForEach(sortedUsers(), id: \.id) { user in
-                            VStack(alignment: .leading) {
-                                Text(user.email)
-                                    .font(.headline)
-                                // Display user role
-                                if user.isAdmin {
-                                    Text("Admin")
-                                        .font(.subheadline)
-                                        .foregroundColor(.red)
-                                } else if user.isStaff {
-                                    // Display staff role
-                                    Text("Staff")
-                                        .font(.subheadline)
-                                        .foregroundColor(.blue)
-                                } else {
-                                    // Display customer role
-                                    Text("Customer")
-                                        .font(.subheadline)
-                                        .foregroundColor(.secondary)
+                        // Display total number of users
+                        Section(header:
+                            Text("Total Users: \(users.count)")
+                                .font(.headline)
+                                .foregroundColor(.primary)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.top, 10)
+                                .padding(.bottom, 10)
+                        ) {
+                            ForEach(sortedUsers(), id: \.id) { user in
+                                VStack(alignment: .leading) {
+                                    Text(user.email)
+                                        .font(.headline)
+                                    
+                                    // Display user role
+                                    if user.isAdmin {
+                                        Text("Admin")
+                                            .font(.subheadline)
+                                            .foregroundColor(.red)
+                                    } else if user.isStaff {
+                                        Text("Staff")
+                                            .font(.subheadline)
+                                            .foregroundColor(.blue)
+                                    } else {
+                                        Text("Customer")
+                                            .font(.subheadline)
+                                            .foregroundColor(.secondary)
+                                    }
+                                    
+                                    if user.loggedIn {
+                                        Text("(Current user)")
+                                            .font(.caption)
+                                            .italic()
+                                            .foregroundColor(.secondary)
+                                    }
+                                }
+                                .swipeActions(edge: .trailing) {
+                                    // Delete only admins or staff
+                                    if (user.isAdmin || user.isStaff) && !user.loggedIn {
+                                        Button(role: .destructive) {
+                                            userToDelete = user
+                                            showDeleteConfirmation = true
+                                        } label: {
+                                            Label("Delete", systemImage: "trash")
+                                        }
+                                    }
                                 }
                             }
                         }
-                        .onDelete(perform: confirmDelete)
                     }
                     .listStyle(InsetGroupedListStyle())
                     .padding(.horizontal, -6)
@@ -60,9 +87,6 @@ struct DisplayAllUsersView: View {
             .navigationTitle("All Users")
             .navigationBarTitleDisplayMode(.inline)
             .background(Color(UIColor.systemGroupedBackground).ignoresSafeArea())
-            .onAppear {
-                fetchAllUsers()
-            }
             .alert("Delete User", isPresented: $showDeleteConfirmation, presenting: userToDelete) { user in
                 Button("Delete", role: .destructive) {
                     deleteUser(user)
@@ -76,33 +100,21 @@ struct DisplayAllUsersView: View {
 
     // MARK: - Helper Functions
 
-    /// Fetches all users from the database
-    private func fetchAllUsers() {
-        let userManager = UserManager(context: context)
-        users = userManager.fetchUsers()
-    }
-
     /// Sorts users into three categories: Admins, Staff and Users
     private func sortedUsers() -> [User] {
         let admins = users.filter { $0.isAdmin }
         let staff = users.filter { $0.isStaff && !$0.isAdmin }
-        let user = users.filter { !$0.isAdmin && !$0.isStaff }.sorted { $0.email.lowercased() < $1.email.lowercased() }
-        return admins + staff + user
-    }
-
-    /// Shows a confirmation alert
-    private func confirmDelete(at offsets: IndexSet) {
-        if let index = offsets.first {
-            userToDelete = sortedUsers()[index]
-            showDeleteConfirmation = true
-        }
+        let customers = users.filter { !$0.isAdmin && !$0.isStaff }.sorted { $0.email.lowercased() < $1.email.lowercased() }
+        return admins + staff + customers
     }
 
     /// Deletes the user from the database
     private func deleteUser(_ user: User) {
-        let userManager = UserManager(context: context)
-        userManager.deleteUser(user)
-        fetchAllUsers()
+        if !user.loggedIn {
+            context.delete(user)
+            
+            try? context.save()
+        }
     }
 }
 
